ca6903fdf50cde39cfffd76ca9c190364a94b89c
chloir <pic.preserve@gmail.com>
2017-10-04 19:39:16 +0900

    First commit for MailerGUI

--- MailerGUI/MainWindow.xaml.cs
+++ MailerGUI/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace MailerGUI
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
     }
 }
